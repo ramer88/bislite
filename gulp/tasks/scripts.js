cad737369523dc19ed28b833ebb8080641fbf5bc
Ahmed Bekov <ramerram@gmail.com>
2018-04-14 11:46:44 +0600

    delete'libsJS:dev'

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -25,14 +25,6 @@ module.exports = function() {
             }));
     });
 
-    $.gulp.task('libsJS:build', () => {
-        return $.gulp.src(['node_modules/jquery/dist/jquery.min.js',
-                           'node_modules/slick-carousel/slick/slick.min.js'])
-            .pipe($.gp.concat('libs.min.js'))
-            .pipe($.gp.uglifyjs())
-            .pipe($.gulp.dest('./build/static/js/'));
-    });
-
     $.gulp.task('js:copy', () => {
         return $.gulp.src(['./dev/static/js/*.js',
                            '!./dev/static/js/libs.min.js'])
